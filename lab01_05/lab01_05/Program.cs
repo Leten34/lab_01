cbc6d43c941ccb6120ff9c2065301f1c241ac27b
Leten34 <63717767+Leten34@users.noreply.github.com>
2020-04-17 18:11:47 +0300

    (init commit)

---
+++ lab01_05/lab01_05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lab01_05
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Please enter perimetr");
+                string temp = Console.ReadLine();
+                double p = Double.Parse(temp);
+                double a = p / 3;
+                double b = a;
+                double c = a;
+                p = p / 2;
+                double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                Console.WriteLine("Storona\tSquar\n{0}\t{1:0.##}", a, S);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("An format exception was thrown: {0}",
+            e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("An DivideByZero exception was thrown: {0}", e.Message);
+            }
+
+        
+        }
+    }
+}
